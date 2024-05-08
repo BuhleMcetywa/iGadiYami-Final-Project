0af8c6d71092fdfb940f7bc4a1cc53d949d76884
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-08 11:08:35 +0200

    First Commit

---
+++ IGadiYamiSln/IGadiYami/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IGadiYami
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
