6bd70ae572c23d8b2f6e475ad317ffc833200248
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-08 14:14:17 +0200

    Carrot & Carrot Disease added to database

--- IGadiYamiSln/IGadiYami/Services/PlantDatabase.cs
+++ IGadiYamiSln/IGadiYami/Services/PlantDatabase.cs
@@ -44,10 +44,10 @@ namespace IGadiYami.Services
                 Carrot carrot = new Carrot()
                 {
                     Name = "Carrot",
-                    SoilType = "Test 1",
-                    WaterIntake = "Test 2L",
-                    SoilPrep = "Test 3 Prep",
-                    TimeToPlant = "Test 4pm"
+                    SoilType = "Fine textured soil with sand",
+                    WaterIntake = "•\tCarrots require consistent watering. Aim for at least 1 inch of water per week (including rain). Overwatering can lead to disease and root rot.",
+                    SoilPrep = "•\tMix aged manure or compost into the soil in the fall or early spring to improve texture.",
+                    TimeToPlant = "•\tIn cooler areas, the best time to plant carrots is from February to April and from August to October. In mild areas, they can be planted throughout the year. •\tBegin to sow the carrot seeds three weeks before the last spring frost."
                 };
                 _dbConnection.Insert(carrot);
             }
@@ -58,23 +58,23 @@ namespace IGadiYami.Services
                     new CarrotDisease()
                     {
                         DiseaseName = "Carrot Rust Fly",
-                        DiseaseDescription = "Testing Disease",
-                        DiseaseCauses = "Testing Causes",
-                        DiseaseControlMessures = "Testing Control Measures"
+                        DiseaseDescription = "",
+                        DiseaseCauses = "The larvae of the carrot rust fly tunnel into the roots, causing damage and making the carrots unmarketable",
+                        DiseaseControlMessures = "To control this pest, you can use floating row covers to prevent adult flies from laying eggs on the plants or consider using appropriate insecticides."
                     },
                     new CarrotDisease()
                     {
-                        DiseaseName = "Carrot Rust Fly 2",
-                        DiseaseDescription = "Testing Disease 2",
-                        DiseaseCauses = "Testing Causes 2",
-                        DiseaseControlMessures = "Testing Control Measures 2"
+                        DiseaseName = "Alternaria Leaf Blight",
+                        DiseaseDescription = "",
+                        DiseaseCauses = "This fungal disease affects the leaves and stems of carrot plants. It causes dark, sunken lesions on the leaves and can lead to defoliation if left untreated.",
+                        DiseaseControlMessures = "Crop rotation, proper sanitation, and the use of fungicides labeled for carrot diseases can help manage Alternaria leaf blight."
                     },
                     new CarrotDisease()
                     {
-                        DiseaseName = "Carrot Rust Fly 3",
-                        DiseaseDescription = "Testing Disease 3",
-                        DiseaseCauses = "Testing Causes 3",
-                        DiseaseControlMessures = "Testing Control Measures 3"
+                        DiseaseName = "Root Knot Nematodes",
+                        DiseaseDescription = "",
+                        DiseaseCauses = "These microscopic worm-like organisms can cause galls or knots to form on the roots of carrot plants. Infected plants may show stunted growth and have reduced yields.",
+                        DiseaseControlMessures = "Crop rotation, soil solarization, and the use of resistant carrot varieties are effective management strategies for root knot nematodes."
                     }
                 };
                 _dbConnection.InsertAll(carrotDiseases);
