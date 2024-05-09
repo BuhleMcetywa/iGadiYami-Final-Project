f1967749cf090a9df30cefad5b32c1f182c7da8c
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-09 20:52:38 +0200

    Camera Page v1.0 done

--- IGadiYamiSln/IGadiYami/ViewModels/CameraPageViewModel.cs
+++ IGadiYamiSln/IGadiYami/ViewModels/CameraPageViewModel.cs
@@ -6,14 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 using IGadiYami.Views;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace IGadiYami.ViewModels
 {
-    public class CameraPageViewModel : BaseViewModel
+    public partial class CameraPageViewModel : BaseViewModel
     {
-        public CameraPageViewModel()
-        {
 
+        [RelayCommand]
+        public async void TakePhoto()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+                    using Stream sourceStream = await photo.OpenReadAsync();
+                    using FileStream localFileStream = File.OpenWrite(localFilePath);
+                    await sourceStream.CopyToAsync(localFileStream);
+                }
+            }
         }
     }
 }
