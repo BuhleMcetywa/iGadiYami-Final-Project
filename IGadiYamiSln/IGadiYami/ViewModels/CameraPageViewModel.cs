35d85ed7af0e3168cb33f137081dffc941ccbf42
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-10 10:13:51 +0200

    Removed camera function

--- IGadiYamiSln/IGadiYami/ViewModels/CameraPageViewModel.cs
+++ IGadiYamiSln/IGadiYami/ViewModels/CameraPageViewModel.cs
@@ -12,23 +12,5 @@ namespace IGadiYami.ViewModels
 {
     public partial class CameraPageViewModel : BaseViewModel
     {
-
-        [RelayCommand]
-        public async void TakePhoto()
-        {
-            if (MediaPicker.Default.IsCaptureSupported)  
-            {
-                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
-
-                if (photo != null)
-                {
-                    // save the file into local storage
-                    string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
-                    using Stream sourceStream = await photo.OpenReadAsync();
-                    using FileStream localFileStream = File.OpenWrite(localFilePath);
-                    await sourceStream.CopyToAsync(localFileStream);
-                }
-            }
-        }
     }
 }
