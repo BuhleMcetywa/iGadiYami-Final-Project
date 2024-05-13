cae25cfbfdad032b488f8240bf8baa38adf485ad
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-13 10:41:13 +0200

    Methods created for user profile database

--- IGadiYamiSln/IGadiYami/Services/ProfileDatabase.cs
+++ IGadiYamiSln/IGadiYami/Services/ProfileDatabase.cs
@@ -1,11 +1,13 @@
 ﻿using IGadiYami.Models.User_Profile;
 using IGadiYami.Views;
 using SQLite;
+using SQLiteNetExtensions.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
 
 namespace IGadiYami.Services
 {
@@ -41,5 +43,33 @@ namespace IGadiYami.Services
             }
         }
 
+        // Methods
+        public void CreatUser(string userName, string userSurname, string userEmail, string userBio, string userPhone, string userImage)
+        {
+            UserProfile userProfile = new UserProfile()
+            {
+                UserName = userName,
+                UserSurname = userSurname,
+                UserEmail = userEmail,
+                UserBio = userBio,
+                UserPhone = userPhone,
+                UserImage = userImage
+            };
+
+            _dbConnection.Insert(userProfile);
+        }
+        public void UpdateUserProfile(UserProfile user)
+        {
+            _dbConnection.Update(user);
+        }
+        public UserProfile GetUserById(int id)
+        {
+            UserProfile user = _dbConnection.Table<UserProfile>().Where(x => x.UserID == id).FirstOrDefault();
+
+            if (user != null)
+                _dbConnection.GetChildren(user, true);
+
+            return user;
+        }
     }
 }
