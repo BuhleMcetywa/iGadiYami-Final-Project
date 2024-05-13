6171b50ad16205e76da876608201cf64baf24efe
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-13 10:32:09 +0200

    profile database created and seeded

---
+++ IGadiYamiSln/IGadiYami/Models/User Profile/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGadiYami.Models.User_Profile
+{
+    public class UserProfile
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserSurname { get; set; }
+        public string UserPhone { get; set; }
+        public string UserBio { get; set; }
+        public string UserImage { get; set; }
+    }
+}
--- IGadiYamiSln/IGadiYami/Services/ProfileDatabase.cs
+++ IGadiYamiSln/IGadiYami/Services/ProfileDatabase.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IGadiYami.Models.User_Profile;
+using IGadiYami.Views;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,35 @@ namespace IGadiYami.Services
 {
     public class ProfileDatabase : IProfileDatabase
     {
+        private SQLiteConnection _dbConnection;
+
+        public ProfileDatabase()
+        {
+            _dbConnection = new SQLiteConnection(GetDatabasePath());
+            _dbConnection.CreateTable<UserProfile>();
+        }
+        public string GetDatabasePath()
+        {
+            string fileName = "profiledatabase.db";
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return pathToDb + fileName;
+        }
+        public void SeedDatabase()
+        { 
+            if (_dbConnection.Table<UserProfile>().Count() == 0)
+            {
+                UserProfile profile = new UserProfile()
+                {
+                    UserName = "Test",
+                    UserSurname = "Test2",
+                    UserEmail = "Test@gmail.com",
+                    UserBio = "Testing",
+                    UserPhone = "12345678910",
+                    UserImage = "no image"
+                };
+                _dbConnection.Insert(profile);
+            }
+        }
+
     }
 }
