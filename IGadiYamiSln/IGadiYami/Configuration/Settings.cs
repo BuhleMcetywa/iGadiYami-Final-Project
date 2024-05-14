2472136bb56df8b18e5df9fc952a467fb14bc168
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-14 21:37:29 +0200

    Model Endpoint, Key & Id added

--- IGadiYamiSln/IGadiYami/Configuration/Settings.cs
+++ IGadiYamiSln/IGadiYami/Configuration/Settings.cs
@@ -8,5 +8,10 @@ namespace IGadiYami.Configuration
 {
     public class Settings : ISettings
     {
+        // Custom Vision Details
+        public static string CustomVisionEndPoint => "https://igadiyamicusomvision.cognitiveservices.azure.com/";
+        public static string PredictionKey => "ea23dda48ae741fe95fdcff27c9bc7ed";
+        public static string ProjectId => "312aeef8-548d-4c7c-840c-01e05059f978";
+        public static string PublishedName => "Plant Disease Detection";
     }
 }
