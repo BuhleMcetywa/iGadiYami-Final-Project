84e08bf4bcfbcd04203ad13872f5323dbe3f1180
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-15 14:55:09 +0200

    User database created & registered

--- IGadiYamiSln/IGadiYami/Services/UserDatabase.cs
+++ IGadiYamiSln/IGadiYami/Services/UserDatabase.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IGadiYami.Models;
+using IGadiYami.Models.UserData;
 
 namespace IGadiYami.Services
 {
-    public class UserDatabase
+    public class UserDatabase : IUserDatabase
     {
+        private SQLiteConnection _dbConnection;
+
+        public UserDatabase()
+        {
+            _dbConnection = new SQLiteConnection(GetDatabasePath());
+            _dbConnection.CreateTable<UserData>();
+        }
+        public string GetDatabasePath()
+        {
+            string fileName = "profiledatabase.db";
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return pathToDb + fileName;
+        }
+        public void SeedDatabase()
+        {
+
+        }
     }
 }
