e62eeb3930f37d42688debed596ad8487dd5df2c
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-15 15:08:57 +0200

    Methods for user database created

--- IGadiYamiSln/IGadiYami/Services/UserDatabase.cs
+++ IGadiYamiSln/IGadiYami/Services/UserDatabase.cs
@@ -17,6 +17,7 @@ namespace IGadiYami.Services
         {
             _dbConnection = new SQLiteConnection(GetDatabasePath());
             _dbConnection.CreateTable<UserData>();
+            SeedDatabase();
         }
         public string GetDatabasePath()
         {
@@ -26,7 +27,56 @@ namespace IGadiYami.Services
         }
         public void SeedDatabase()
         {
+            if (_dbConnection.Table<UserData>().Count() == 0)
+            {
+                UserData userData = new UserData()
+                {
+                    UserName = "Test Name",
+                    UserSurname = "Test Surname",
+                    UserEmail = "Test@gmail.com",
+                    UserPassword = "Test Password",
+                    UserPhoneNumber = "1234567890"
+                };
+                _dbConnection.Insert(userData);
+            }
+        }
 
+        // Methods
+        public void CreateUser(string userName, string userSurname, string userEmail, string userPassword, string userPhoneNumber)
+        {
+            UserData user = new UserData()
+            {
+                UserName = userName,
+                UserSurname = userSurname,
+                UserEmail = userEmail,
+                UserPassword = userPassword,
+                UserPhoneNumber = userPhoneNumber
+            };
+            _dbConnection.Insert(user);
+        }
+        public void UpdateUser(int userId, string userName, string userSurname, string userEmail, string userPassword, string userPhoneNumber)
+        {
+            var userToUpdate = _dbConnection.Table<UserData>().FirstOrDefault(u => u.UserID == userId);
+
+            if (userToUpdate != null)
+            {
+                userToUpdate.UserName = userName;
+                userToUpdate.UserSurname = userSurname;
+                userToUpdate.UserEmail = userEmail;
+                userToUpdate.UserPassword = userPassword;
+                userToUpdate.UserPhoneNumber = userPhoneNumber;
+
+                _dbConnection.Update(userToUpdate);
+            }
+        }
+        public void DeleteUser(int userId)
+        {
+            var userToDelete = _dbConnection.Table<UserData>().FirstOrDefault(u => u.UserID == userId);
+
+            if (userToDelete != null)
+            {
+                _dbConnection.Delete<UserData>(userId);
+            }
         }
     }
 }
