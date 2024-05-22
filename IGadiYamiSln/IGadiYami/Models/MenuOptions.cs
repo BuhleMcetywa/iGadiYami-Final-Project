5f793921d78d855c11e0f50d11b922a7e8d857a2
5100152 <5100152@mypillar7uwc.ac.za>
2024-05-22 12:42:56 +0200

    added a functionless main menu with swipe gestures

---
+++ IGadiYamiSln/IGadiYami/Models/MenuOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGadiYami.Models
+{
+    public class MenuOptions
+    {
+        public string OptionName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
