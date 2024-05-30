39ab59afc239bae4aaf5e30927e8bf89210e01d5
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-30 17:50:42 +0200

    Community Page Logic done

--- IGadiYamiSln/IGadiYami/ViewModels/CommunityPageViewModel.cs
+++ IGadiYamiSln/IGadiYami/ViewModels/CommunityPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,29 @@ using System.Threading.Tasks;
 
 namespace IGadiYami.ViewModels
 {
-    public partial class CommunityPageViewModel:BaseViewModel
+    public partial class CommunityPageViewModel : BaseViewModel
     {
+		private bool _ismessagevisible;
+		public bool IsMessageVisible
+		{
+			get { return _ismessagevisible; }
+			set { _ismessagevisible = value; OnPropertyChanged(); }
+		}
 
-    }
+		public CommunityPageViewModel()
+		{
+            IsMessageVisible = false;
+		}
+
+        public void MakeMessageVisible()
+		{
+            IsMessageVisible = true;
+		}
+
+		[RelayCommand]
+		public void ShowMessage()
+		{
+			MakeMessageVisible();
+		}
+	}
 }
