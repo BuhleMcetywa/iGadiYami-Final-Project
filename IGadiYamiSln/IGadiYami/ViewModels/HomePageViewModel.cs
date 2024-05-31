c6c5b1a4884cc217822e357b35f8b3b591d63d78
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-05-31 11:09:02 +0200

    Home page message 78% working

--- IGadiYamiSln/IGadiYami/ViewModels/HomePageViewModel.cs
+++ IGadiYamiSln/IGadiYami/ViewModels/HomePageViewModel.cs
@@ -1,16 +1,61 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using IGadiYami.Models;
+using IGadiYami.Services;
 
 namespace IGadiYami.ViewModels
 {
     public partial class HomePageViewModel : BaseViewModel
     {
+        SignUpPageViewModel _signUpPageViewModel;
+        private UserDatabase _userdatabase;
         public IList<MenuOptions> Menu { get; private set; }
 
-
-        public HomePageViewModel()
+        private string _displayname;
+        public string DisplayName
         {
+            get { return _displayname; }
+            set { _displayname = value; OnPropertyChanged(); }
+        }
+        private string _displaycurrenttime;
+
+        public string DisplayCurrentTime
+        {
+            get { return _displaycurrenttime; }
+            set { _displaycurrenttime = value; OnPropertyChanged(); }
+        }
+
+
+
+        public HomePageViewModel(UserDatabase userDatabase)
+        {
+            _userdatabase = userDatabase;
             CreateMenuCollection();
+            //GetUserNameToDisplay();
+            GetCurrenTime();
+        }
+        public void GetCurrenTime()
+        {
+            //var signupentry = _signUpPageViewModel.Name;
+            var name = _userdatabase.GetUserByName("Ethan");
+            DisplayName = name.UserName; 
+
+            var currentTime = DateTime.Now;
+            var timeString = currentTime.ToString("HH:mm:ss tt");
+
+            int hour = currentTime.Hour;
+
+            if (hour < 12)
+            {
+                DisplayCurrentTime = $"Good Morning! {DisplayName}.";
+            }
+            else if (hour < 18)
+            {
+                DisplayCurrentTime = $"Good Afternoon! {DisplayName}.";
+            }
+            else
+            {
+                DisplayCurrentTime = $"Good Evening! {DisplayName}.";
+            }
         }
 
         [RelayCommand]
@@ -33,6 +78,7 @@ namespace IGadiYami.ViewModels
         {
             await Shell.Current.GoToAsync("croptracking");
         }
+
 
         void CreateMenuCollection() // Changed return type to void
         {
